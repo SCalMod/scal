bd0d647541e92588fd5776f00f07dd7615437e72
DatYinYang <jaewonmichael@gmail.com>
2013-12-18 18:56:09 -0800

    VariableHandler
    Started setting up VariableHander class
    PreAlpha 0.1.0

---
+++ common/VariableHandler.java
@@ -0,0 +1,10 @@
+package scal.common;
+
+public class VariableHandler 
+{
+	public static int hitMarkerTimer = 0;
+	public static int shootInterval = 0;
+	public static int reloadIntercval = 0;
+	public static float zoomLevel = 1.0f;
+	public static float reticleScale = 1.0f;
+}
